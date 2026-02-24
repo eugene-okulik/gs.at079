ff32819e73da3d8d09523681d893cfbca7cc00b5
George-GS <georgegarber11@gmail.com>
2026-02-24 13:08:19 +0300

    hW5 task2 добавил пробелы вокруг операторов

--- homework/egor_shabarov/Homework_5/task2.py
+++ homework/egor_shabarov/Homework_5/task2.py
@@ -3,13 +3,13 @@ res_2 = 'результат операции: 514'
 res_3 = 'результат работы программы: 9'
 
 ind_space = res_1.index(': ')
-num_1 = int(res_1[ind_space+1:])
+num_1 = int(res_1[ind_space + 1:])
 
 ind_space = res_2.index(': ')
-num_2 = int(res_2[ind_space+1:])
+num_2 = int(res_2[ind_space + 1:])
 
 ind_space = res_3.index(': ')
-num_3 = int(res_3[ind_space+1:])
+num_3 = int(res_3[ind_space + 1:])
 
 print(num_1 + 10)
 print(num_2 + 10)
