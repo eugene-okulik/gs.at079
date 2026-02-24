10ebfefe0e962de85c87105c58a577dc8c20ff17
George-GS <georgegarber11@gmail.com>
2026-02-24 13:06:00 +0300

    hw5 доработал task2, сделал получение результата с помощью метода index и срезов

--- homework/egor_shabarov/Homework_5/task2.py
+++ homework/egor_shabarov/Homework_5/task2.py
@@ -2,10 +2,15 @@ res_1 = 'результат операции: 42'
 res_2 = 'результат операции: 514'
 res_3 = 'результат работы программы: 9'
 
-text_1, num_1 = res_1.split(': ')
-text_2, num_2 = res_2.split(': ')
-text_3, num_3 = res_3.split(': ')
+ind_space = res_1.index(': ')
+num_1 = int(res_1[ind_space+1:])
 
-print(int(num_1) + 10)
-print(int(num_2) + 10)
-print(int(num_3) + 10)
+ind_space = res_2.index(': ')
+num_2 = int(res_2[ind_space+1:])
+
+ind_space = res_3.index(': ')
+num_3 = int(res_3[ind_space+1:])
+
+print(num_1 + 10)
+print(num_2 + 10)
+print(num_3 + 10)
