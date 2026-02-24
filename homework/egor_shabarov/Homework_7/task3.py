31c901ad26b0fda0bbd2e228e716a88c59ad1a35
George-GS <georgegarber11@gmail.com>
2026-02-24 22:07:22 +0300

    homework 7 done

---
+++ homework/egor_shabarov/Homework_7/task3.py
@@ -0,0 +1,11 @@
+result_1 = 'результат операции: 42'
+result_2 = 'результат операции: 54'
+result_3 = 'результат работы программы: 209'
+result_4 = 'результат: 2'
+
+
+def my_func(result_program: str):
+    print(int(result_program.split(': ')[1]) + 10)
+
+
+my_func(result_1)
