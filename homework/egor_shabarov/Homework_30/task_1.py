8131d3311d042acf42add48d64a037b421351861
George-GS <georgegarber11@gmail.com>
2026-04-09 22:01:41 +0300

    homework 30 done

---
+++ homework/egor_shabarov/Homework_30/task_1.py
@@ -0,0 +1,32 @@
+import json
+
+from playwright.sync_api import Page, expect, Request, Route, Response
+
+import re
+from time import sleep
+
+
+def test_one(page: Page):
+
+    new_name_prod = 'Яблокофон 17 про'
+
+    def handle_route(route: Route):
+        response = route.fetch()
+        body = response.json()
+        body['body']['digitalMat'][0]['familyTypes'][0]['productName'] = new_name_prod
+        body["body"]["digitalMat"][0]["familyTypes"][0]["tabTitle"] = new_name_prod
+        body = json.dumps(body)
+
+        route.fulfill(response=response, body=body)
+
+    page.route(re.compile('api/digital-mat'), handle_route)
+    page.goto('https://www.apple.com/shop/buy-iphone')
+
+    page.get_by_role('heading', name='iPhone 17 Pro & iPhone 17 Pro Max').click()
+
+    # heading = page.locator('#rf-digitalmat-overlay-label-0').nth(0)
+    heading = page.get_by_role('heading', name=new_name_prod, exact=True)
+
+    expect(heading).to_be_visible()
+    expect(heading).to_have_text(new_name_prod)
+    sleep(5)
