e564d966189b5eefc4a9e9b52581163e1dad5e2f
George-GS <georgegarber11@gmail.com>
2026-04-14 00:19:42 +0300

    homework 34 done

---
+++ homework/egor_shabarov/Homework_34/gs_app.py
@@ -0,0 +1,10 @@
+import datetime
+
+print("Программа запущена")
+print(f"Текущее время: {datetime.datetime.now()}")
+
+for i in range(5):
+    print(i)
+
+print("Программа завершена")
+print(f"Текущее время: {datetime.datetime.now()}")
